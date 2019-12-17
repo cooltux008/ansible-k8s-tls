29c98567adabfd5c57c944eb6a96dae35d0623bd
Jun Liu <liujun_live@msn.com>
2019-12-17 18:08:05 +0800

    tools update

--- tools/kubeconfig_gen.sh
+++ tools/kubeconfig_gen.sh
@@ -24,18 +24,15 @@ kubectl config set-cluster kubernetes \
   --embed-certs=true \
   --server=${KUBE_APISERVER} \
   --kubeconfig=$tls_dir/bootstrap.kubeconfig
-
 # 设置客户端认证参数
 kubectl config set-credentials kubelet-bootstrap \
   --token=${BOOTSTRAP_TOKEN} \
   --kubeconfig=$tls_dir/bootstrap.kubeconfig
-
 # 设置上下文参数
 kubectl config set-context default \
   --cluster=kubernetes \
   --user=kubelet-bootstrap \
   --kubeconfig=$tls_dir/bootstrap.kubeconfig
-
 # 设置默认上下文
 kubectl config use-context default --kubeconfig=$tls_dir/bootstrap.kubeconfig
 
