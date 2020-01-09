4893d1d19523a86c68df42f098ec93dd3aa4d79f
Jun Liu <liujun_live@msn.com>
2020-01-10 02:40:04 +0800

    debian 适配containerd ok

--- tools/etcd_tls2base64.sh
+++ tools/etcd_tls2base64.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p ../dev/group_vars
-cat > ../dev/group_vars/etcd <<-EOF
+cat > ../dev/group_vars/etcd.yml <<-EOF
 etcd_cert: $(cat ../tls/etcd/etcd-client.pem|base64)
 etcd_key: $(cat ../tls/etcd/etcd-client-key.pem|base64)
 etcd_ca: $(cat ../tls/etcd/etcd-ca.pem|base64)
 EOF
-cat ../dev/group_vars/etcd
+cat ../dev/group_vars/etcd.yml
